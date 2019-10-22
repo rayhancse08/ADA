58f25132c5618e33427e968591cf1223bab854b3
Rayhan <rayhancse08@gmail.com>
2019-10-22 15:51:55 +0600

    change viewset permission

--- tracking/views.py
+++ tracking/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from tracking.models import PlayHub
 from tracking.serializer import TrackingSerializer
+from rest_framework import viewsets, mixins
 
 
 # Create your views here.
 
-class TrackingViewSet(viewsets.ModelViewSet):
+class TrackingViewSet(mixins.CreateModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet,
+                      mixins.ListModelMixin):
     queryset = PlayHub.objects.all()
     serializer_class = TrackingSerializer
